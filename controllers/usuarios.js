ac8ae516c22413da171cf116c74891716e91ad32
villaa <villaa@epsas.com.bo>
2020-11-17 10:13:42 -0400

    Fin seccion 11

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -8,10 +8,23 @@ const {generarJWT} = require('../helpers/jwt');
 
 const getUsuarios = async (req,res)=>{
 
-    const usuarios = await Usuario.find({},'nombre email role google');
+    const desde = Number(req.query.desde) || 0;
+    //console.log(desde);
+   /*
+    const usuarios = await Usuario.find({},'nombre email role google')
+                                  .skip(desde)
+                                  .limit(5);
+    const total = await Usuario.count(); */
+     const [usuarios,total] = await Promise.all([Usuario.find({},'nombre email role google img')
+                                           .skip(desde)
+                                           .limit(5),
+                       Usuario.countDocuments()
+                    ]
+                    );                           
     res.json({
        ok:true,
-       usuarios       
+       usuarios,
+       total       
        //uid:req.uid
     });
 }
---
+++ helpers/actualizar-imagen.js
@@ -0,0 +1,62 @@
+const Usuario = require('../models/usuario');
+const Medico = require('../models/medico');
+const Hospital = require('../models/hospital');
+
+const borrarImagen = (path)=>{
+    
+               if(fs.existsSync(path)){
+                   //borrar la imagen anterior
+                   fs.unlinkSync(path);
+               }
+}
+
+const actualizarImagen = async (tipo,id,nombreArchivo) => {
+    let pathViejo = '';
+       switch (tipo) {
+           case 'medicos':
+               const medico = await Medico.findById(id);
+               if(!medico){
+                   console.log('No es un medico por id');
+                  return false;               
+               } 
+               pathViejo = `./uploads/medicos/${medico.img}`;
+               borrarImagen(pathViejo);
+              
+               medico.img = nombreArchivo;
+               await medico.save();
+               return true;
+               break;       
+           case 'hospitales':                              
+                const hospital = await Hospital.findById(id);
+                console.log(hospital);
+                if(!hospital){
+                 console.log('No es un hospital por id');
+                 return false;               
+                } 
+                pathViejo = `./uploads/hospitales/${hospital.img}`;                
+                borrarImagen(pathViejo);          
+                hospital.img = nombreArchivo;
+                await hospital.save();
+                return true;
+                break;
+            case 'usuarios':
+                const usuario = await Usuario.findById(id);
+                if(!usuario){
+                 console.log('No es un usuario por id');
+                 return false;               
+                } 
+                pathViejo = `./uploads/usuarios/${hospital.img}`;
+                borrarImagen(pathViejo);          
+                usuario.img = nombreArchivo;
+                await usuario.save();
+                return true;               
+                break;     
+           default:
+                break;
+       }
+
+}
+
+module.exports = {
+    actualizarImagen
+}
